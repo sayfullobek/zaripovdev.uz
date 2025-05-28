11aff8c8e2d87441f496a527e0fee6ae16bd953d
sayfullobek <sayfullogithub@gmail.com>
2025-05-28 07:24:03 -0400

    mm

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import React from "react"
-import "./App.css";
-import Header from "./components/header/Header";
-import Home from "./components/Home/Home";
-import About from "./components/about/About";
-import Skills from "./components/skills/Skills";
-import Qualification from "./components/qualification/Qualification";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
-import ScrollUp from "./components/scrollUp/ScrollUp";
-
-
+import './App.css'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import Qualification from './components/qualification/Qualification'
+import ScrollUp from './components/scrollUp/ScrollUp'
+import Skills from './components/skills/Skills'
 
 function App() {
-  return (
-    <>
-        <Header/>
-        <main className="main">
-            <Home/>
-            <About/>
-            <Skills/>
-            <Qualification/>
-            <Contact/>
-           
-        </main>
+	return (
+		<>
+			<Header />
+			<main className='main'>
+				<Home />
+				<About />
+				<Skills />
+				<Qualification />
+				<Contact />
+			</main>
 
-         <Footer/>
-         <ScrollUp/>
-    </>
-  )
+			<Footer />
+			<ScrollUp />
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import "./home.css"
-import Social from './Social'  
 import Data from './Data'
-import ScrollDown from './ScrollDown';
+import './home.css'
+import ScrollDown from './ScrollDown'
+import Social from './Social'
 
 const Home = () => {
-  return (
-    <section className="home section" id="home">
-        <div className="home__container container grid">
-            <div className="home__content grid">
-                <Social/>
+	return (
+		<section className='home section' id='home'>
+			<div className='home__container container grid'>
+				<div className='home__content grid'>
+					<Social />
 
-                <div className="home__img"></div>
+					<div className='home__img'></div>
 
-                <Data/>
-            </div>
-            <ScrollDown/>
-,
-
-        </div>
-    </section>
-  )
+					<Data />
+				</div>
+				<ScrollDown />,
+			</div>
+		</section>
+	)
 }
 
 export default Home
